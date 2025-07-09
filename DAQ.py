8657f4b5fddb4a0719e3648e9ccebc465a90bf5f
Nate-4-4 <153030151+Nate-4-4@users.noreply.github.com>
2025-07-09 15:42:28 -0400

    working graph

--- DAQ.py
+++ DAQ.py
@@ -1,6 +1,36 @@
 import nidaqmx
+import matplotlib.pyplot as plt
+import time
+from nidaqmx.constants import TerminalConfiguration
 
-print("Hello World")
-with nidaqmx.Task() as task:
-    pass
-print("Test")
+#setup NI DAQ
+task = nidaqmx.Task()
+task.ai_channels.add_ai_voltage_chan("Dev1/ai0", terminal_config=TerminalConfiguration.DIFFERENTIAL)
+
+#setup plot
+plt.ion()
+figure, axis = plt.subplots()
+xData = []
+yData = []
+line, = axis.plot(xData, yData)
+axis.set_xlabel("time")
+axis.set_ylabel("voltage")
+axis.set_ylim(0,5)
+
+print("Startup")
+initialTime = time.time()
+try:
+    while(True):
+        data = task.read()
+        yData.append(data)
+        xData.append(time.time())
+        line.set_xdata(xData)
+        line.set_ydata(yData)
+        axis.relim()
+        axis.autoscale_view()
+        plt.pause(0.05)
+except KeyboardInterrupt:
+    print("Shutting Down")
+finally:
+    task.close()
+    print("Shutdown Complete")
